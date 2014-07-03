0fd978fd1cf9a79d28195674c3a04d8abdf33b50
Carl Martens <carlrmartens@gmail.com>
2014-07-03 15:58:39 -0500

    Added foundation to the yeti theme

--- skin/frontend/yeti/default/config.rb
+++ skin/frontend/yeti/default/config.rb
@@ -1,10 +1,26 @@
+# Require any additional compass plugins here.
+add_import_path "components/foundation/scss"
+
 # note: this should never truly be refernced since we are using relative assets
-http_path = "/skin/frontend/rwd/default/"
-css_dir = "/css"
-sass_dir = "/scss"
-images_dir = "/images"
-javascripts_dir = "/js"
+http_path = "/skin/frontend/yeti/default/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
 relative_assets = true
 
-output_style = :expanded
-environment = :production
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
