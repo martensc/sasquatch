854135d0ecc82d3f44c578ef9f1ce67cdedf6c6d
Carl Martens <carlrmartens@gmail.com>
2014-10-04 20:34:58 -0500

    Added gulpfile

---
+++ skin/frontend/yeti/yeti/Gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var prefix = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+
+// Paths
+var paths = {
+    scripts: ['js/*.js'],
+    images: ['images/**'],
+};
+
+// Compile Sass
+gulp.task('sass', function() {
+    gulp.src(['scss/**/*.scss'])
+        .pipe(plumber())
+        .pipe(sass({
+            includePaths: ['scss', 'bower_components/foundation/scss'],
+            outputStyle: 'expanded'
+        }))
+        .pipe(prefix(
+            "last 1 version", "> 1%", "ie 8", "ie 7"
+        ))
+        .pipe(gulp.dest('css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css'));
+});
+
+// Watch files
+gulp.task('watch', function(event) {
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
